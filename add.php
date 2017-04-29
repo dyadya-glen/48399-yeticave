16f13c2dbce827a950aaaf0e22c5675dc3d1bb31
Roman Gulyaev <glen2001@yandex.ru>
2017-04-30 00:53:53 +0300

    #13 Получение данных из форм v1.0

---
+++ add.php
@@ -0,0 +1,73 @@
+<?php
+
+require 'functions.php';
+
+include 'data.php';
+
+$errors = [];
+
+if (!empty($_POST)) {
+    foreach ($_POST as $key => $value) {
+        $_POST[$key] = strip_tags($value);
+
+        if (empty($value)) {
+            $errors[$key] = 'Заполните это поле';
+            continue;
+        }
+
+        if (in_array($key, ['lot-rate', 'lot-step']) && !filter_var($value, FILTER_VALIDATE_INT)) {
+            $errors[$key] = 'Введите число';
+        }
+    }
+
+    if (isset($_FILES['uploadfile'])) {
+        $photoLot = $_FILES['uploadfile'];
+
+        if ($photoLot['type'] == 'image/jpeg') {
+            move_uploaded_file($photoLot['tmp_name'], 'img/' . $photoLot['name']);
+        } else {
+            $errors['uploadfile'] = 'Неверный формат!';
+        }
+    }
+
+    if (empty($errors)) {
+        array_unshift(
+            $bulletin_board,
+            [
+                "name" => $_POST["lot-name"],
+                "category" => $_POST["category"],
+                "price" => $_POST["lot-rate"],
+                "url_image" => 'img/' . $photoLot['name'],
+            ]
+        );
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>DC Ply Mens 2016/2017 Snowboard</title>
+    <link href="css/normalize.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+
+<?= includeTemplate('header.php', []); ?>
+
+<?php if (empty($_POST) || !empty($errors)) : ?>
+
+    <?= includeTemplate('add_lot.php', ['errors' => $errors, 'photoLot' => $photoLot, 'categories' => $categories]); ?>
+
+<?php else : ?>
+
+    <?= includeTemplate('main.php', ['categories' => $categories, 'bulletin_board' => $bulletin_board]); ?>
+
+<?php endif; ?>
+
+<?= includeTemplate('footer.php', []); ?>
+
+</body>
+</html>
--- data.php
+++ data.php
@@ -1,4 +1,6 @@
 <?php
+$categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
+
 
 $bulletin_board = [
     [
@@ -36,5 +38,28 @@ $bulletin_board = [
         "category" => "Разное",
         "price" => "5400",
         "url_image" => "img/lot-6.jpg",
+    ],
+];
+
+$bets = [
+    [
+        'name' => 'Иван',
+        'price' => 11500,
+        'ts' => strtotime('-' . rand(1, 50) .' minute'),
+    ],
+    [
+        'name' => 'Константин',
+        'price' => 11000,
+        'ts' => strtotime('-' . rand(1, 18) .' hour'),
+    ],
+    [
+        'name' => 'Евгений',
+        'price' => 10500,
+        'ts' => strtotime('-' . rand(25, 50) .' hour'),
+    ],
+    [
+        'name' => 'Семён',
+        'price' => 10000,
+        'ts' => strtotime('last week'),
     ],
 ];
